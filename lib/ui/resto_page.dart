58a8e53422dd999a34fb5fe59f27ff5399d45bdc
fityanrahman <fi.elrahman6@gmail.com>
2023-05-22 14:45:07 +0700

    improved tambah makanan button; added fav makanan button

--- lib/ui/resto_page.dart
+++ lib/ui/resto_page.dart
@@ -11,6 +11,7 @@ class RestaurantPage extends StatefulWidget {
 
 class _RestaurantPageState extends State<RestaurantPage> {
   var menu = 0;
+  var fav = false;
 
   @override
   Widget build(BuildContext context) {
@@ -19,104 +20,123 @@ class _RestaurantPageState extends State<RestaurantPage> {
     return Scaffold(
       body: SafeArea(
         child: SingleChildScrollView(
-          child: Column(
-            children: [
-              Container(
-                width: double.infinity,
-                padding: const EdgeInsets.symmetric(vertical: 12),
-                decoration: const BoxDecoration(
-                  border: Border.symmetric(
-                    horizontal: BorderSide(
-                      color: Color(0xffd9d9d9),
-                    ),
+          child: _itemRestoWidget(textTheme),
+        ),
+      ),
+    );
+  }
+
+  Widget _itemRestoWidget(TextTheme textTheme) {
+    return Column(
+          children: [
+            Container(
+              width: double.infinity,
+              padding: const EdgeInsets.symmetric(vertical: 12),
+              decoration: const BoxDecoration(
+                border: Border.symmetric(
+                  horizontal: BorderSide(
+                    color: Color(0xffd9d9d9),
                   ),
                 ),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    ListTile(
-                      leading: ClipRRect(
-                        borderRadius: const BorderRadius.all(
-                          Radius.circular(8),
-                        ),
-                        child: Image.network(
-                          'https://restaurant-api.dicoding.dev/images/medium/14',
-                          fit: BoxFit.cover,
-                          errorBuilder: (ctx, error, _) =>
-                              const Center(child: Icon(Icons.error)),
-                        ),
+              ),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  ListTile(
+                    leading: ClipRRect(
+                      borderRadius: const BorderRadius.all(
+                        Radius.circular(8),
                       ),
-                      title: const Text('Kari kacang dan telur'),
-                      subtitle: const Text('Rp 10.000'),
+                      child: Image.network(
+                        'https://restaurant-api.dicoding.dev/images/medium/14',
+                        fit: BoxFit.cover,
+                        errorBuilder: (ctx, error, _) =>
+                            const Center(child: Icon(Icons.error)),
+                      ),
                     ),
-                    menu == 0
-                        ? Padding(
-                            padding: EdgeInsets.symmetric(horizontal: 16),
-                            child: ElevatedButton(
-                              onPressed: () {
-                                setState(() {
-                                  menu++;
-                                  print('menu : $menu');
-                                });
-                              },
-                              child: Text(
-                                'Tambah',
-                                style: textTheme.labelSmall,
-                              ),
-                            ),
-                          )
-                        : Row(
-                            children: [
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  shape: CircleBorder(),
-                                  minimumSize: Size.zero,
-                                  fixedSize: Size(24, 24),
-                                  padding: EdgeInsets.zero,
-                                ),
-                                onPressed: () {
-                                  setState(() {
-                                    menu--;
-                                    print('menu : $menu');
-                                  });
-                                },
-                                child: Icon(
-                                  Icons.remove,
-                                  size: 16,
-                                ),
-                              ),
-                              Padding(
-                                padding:
-                                    const EdgeInsets.symmetric(horizontal: 12),
-                                child: Text(menu.toString()),
-                              ),
-                              ElevatedButton(
-                                style: ElevatedButton.styleFrom(
-                                  shape: CircleBorder(),
-                                  minimumSize: Size.zero,
-                                  fixedSize: Size(24, 24),
-                                  padding: EdgeInsets.zero,
-                                ),
+                    title: const Text('Kari kacang dan telur'),
+                    subtitle: const Text('Rp 10.000'),
+                  ),
+                  Padding(
+                    padding:
+                        EdgeInsets.only(left: menu == 0 ? 16 : 0, right: 16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        menu == 0
+                            ? ElevatedButton(
                                 onPressed: () {
                                   setState(() {
                                     menu++;
                                     print('menu : $menu');
                                   });
                                 },
-                                child: Icon(
-                                  Icons.add,
-                                  size: 16,
+                                child: Text(
+                                  'Tambah',
+                                  style: textTheme.labelSmall,
                                 ),
+                              )
+                            : Row(
+                                children: [
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      shape: CircleBorder(),
+                                      minimumSize: Size.zero,
+                                      fixedSize: Size(24, 24),
+                                      padding: EdgeInsets.zero,
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        menu--;
+                                        print('menu : $menu');
+                                      });
+                                    },
+                                    child: Icon(
+                                      Icons.remove,
+                                      size: 16,
+                                    ),
+                                  ),
+                                  Padding(
+                                    padding: const EdgeInsets.symmetric(
+                                        horizontal: 12),
+                                    child: Text(menu.toString()),
+                                  ),
+                                  ElevatedButton(
+                                    style: ElevatedButton.styleFrom(
+                                      shape: CircleBorder(),
+                                      minimumSize: Size.zero,
+                                      fixedSize: Size(24, 24),
+                                      padding: EdgeInsets.zero,
+                                    ),
+                                    onPressed: () {
+                                      setState(() {
+                                        menu++;
+                                        print('menu : $menu');
+                                      });
+                                    },
+                                    child: Icon(
+                                      Icons.add,
+                                      size: 16,
+                                    ),
+                                  ),
+                                ],
                               ),
-                            ],
-                          ),
-                  ],
-                ),
+                        InkWell(
+                          onTap: () {
+                            setState(() {
+                              fav = !fav;
+                              print(fav);
+                            });
+                          },
+                          child: Icon(fav ? Icons.star : Icons.star_outline, color: Colors.orangeAccent,),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
               ),
-            ],
-          ),
-        ),
-      ),
-    );
+            ),
+          ],
+        );
   }
 }
