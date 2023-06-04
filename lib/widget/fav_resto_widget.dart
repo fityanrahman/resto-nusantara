4527f366e0ff6fe5b760612392d78c309d326b34
fityanrahman <fi.elrahman6@gmail.com>
2023-06-04 08:39:23 +0700

    added gradient overlay fav_resto_widget

--- lib/widget/fav_resto_widget.dart
+++ lib/widget/fav_resto_widget.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:submission_resto/common/funs/get_color_scheme.dart';
 import 'package:submission_resto/data/model/restaurants_model.dart';
 
 class FavRestoWidget extends StatelessWidget {
@@ -11,11 +12,13 @@ class FavRestoWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    ColorScheme colorScheme = getCurrentColorScheme(context);
+
     return AspectRatio(
       aspectRatio: 1,
       child: Container(
         height: 280,
-        padding: const EdgeInsets.all(24),
+        // padding: const EdgeInsets.all(24),
         alignment: Alignment.bottomLeft,
         clipBehavior: Clip.hardEdge,
         decoration: BoxDecoration(
@@ -24,23 +27,47 @@ class FavRestoWidget extends StatelessWidget {
             Radius.circular(24),
           ),
           image: DecorationImage(
-            image: NetworkImage(
-                restaurants.pictureId!),
+            image: NetworkImage(restaurants.pictureId!),
             fit: BoxFit.cover,
           ),
         ),
-        child: Column(
-          mainAxisSize: MainAxisSize.min,
-          crossAxisAlignment: CrossAxisAlignment.start,
+        child: Stack(
           children: [
-            Text(restaurants.name!),
-            Text(restaurants.city!),
-            Row(
-              children: [
-                const Icon(Icons.star),
-                Text(restaurants.rating.toString()),
-              ],
-            )
+            Positioned.fill(
+              child: Container(
+                decoration: BoxDecoration(
+                  gradient: LinearGradient(
+                    colors: [
+                      colorScheme.secondaryContainer,
+                      Colors.transparent,
+                      Colors.transparent,
+                      colorScheme.secondaryContainer,
+                    ],
+                    begin: Alignment.topCenter,
+                    end: Alignment.bottomCenter,
+                    stops: const [0, 0, 0.1, 1],
+                  ),
+                ),
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(20.0),
+              child: Column(
+                mainAxisSize: MainAxisSize.max,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                mainAxisAlignment: MainAxisAlignment.end,
+                children: [
+                  Text(restaurants.name!, style: TextStyle(color: colorScheme.onSecondaryContainer),),
+                  Text(restaurants.city!),
+                  Row(
+                    children: [
+                      const Icon(Icons.star),
+                      Text(restaurants.rating.toString()),
+                    ],
+                  )
+                ],
+              ),
+            ),
           ],
         ),
       ),
